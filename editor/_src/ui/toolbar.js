7f99637f44859030173bde7fb990d10ef26171f4
朱得 <1153468993@qq.com>
2025-03-16 16:14:43 +0800

    refactor(editor): 重构工具栏组件
    - 将 cls_uiToolbar 类中的方法从原型对象中移至类定义内部
    - 优化了代码结构，提高了代码的可读性和维护性
    - 保留了原有的功能和逻辑

--- editor/_src/ui/toolbar.js
+++ editor/_src/ui/toolbar.js
@@ -2,6 +2,8 @@ import uiUtils from "./uiutils.js";
 import cls_UIBase from "./UIBase.cls.js";
 
 class cls_uiToolbar extends cls_UIBase {
+	items = null;
+
 	/**
 	 * 构造函数
 	 */
@@ -11,45 +13,46 @@ class cls_uiToolbar extends cls_UIBase {
 		this.initOptions(options);
 		this.initToolbar();
 	}
+	
+	initToolbar() {
+		this.items = this.items || [];
+		this.initUIBase();
+	};
+	add(item, index) {
+		if (index === undefined) {
+			this.items.push(item);
+		} else {
+			this.items.splice(index, 0, item);
+		}
+	};
+	getHtmlTpl() {
+		var buff = [];
+		for (var i = 0; i < this.items.length; i++) {
+			buff[i] = this.items[i].renderHtml();
+		}
+		return (
+			'<div id="##" class="edui-toolbar %%" onselectstart="return false;" onmousedown="return $$._onMouseDown(event, this);">' +
+			buff.join("") +
+			"</div>"
+		);
+	};
+	postRender() {
+		var box = this.getDom();
+		for (var i = 0; i < this.items.length; i++) {
+			this.items[i].postRender();
+		}
+		uiUtils.makeUnselectable(box);
+	};
+	_onMouseDown(e) {
+		var target = e.target || e.srcElement,
+			tagName = target && target.tagName && target.tagName.toLowerCase();
+		if (tagName == "input" || tagName == "object" || tagName == "object") {
+			return false;
+		}
+	};
 }
 
-cls_uiToolbar.prototype.items = null;
-cls_uiToolbar.prototype.initToolbar = function () {
-	this.items = this.items || [];
-	this.initUIBase();
-};
-cls_uiToolbar.prototype.add = function (item, index) {
-	if (index === undefined) {
-		this.items.push(item);
-	} else {
-		this.items.splice(index, 0, item);
-	}
-};
-cls_uiToolbar.prototype.getHtmlTpl = function () {
-	var buff = [];
-	for (var i = 0; i < this.items.length; i++) {
-		buff[i] = this.items[i].renderHtml();
-	}
-	return (
-		'<div id="##" class="edui-toolbar %%" onselectstart="return false;" onmousedown="return $$._onMouseDown(event, this);">' +
-		buff.join("") +
-		"</div>"
-	);
-};
-cls_uiToolbar.prototype.postRender = function () {
-	var box = this.getDom();
-	for (var i = 0; i < this.items.length; i++) {
-		this.items[i].postRender();
-	}
-	uiUtils.makeUnselectable(box);
-};
-cls_uiToolbar.prototype._onMouseDown = function (e) {
-	var target = e.target || e.srcElement,
-		tagName = target && target.tagName && target.tagName.toLowerCase();
-	if (tagName == "input" || tagName == "object" || tagName == "object") {
-		return false;
-	}
-};
+
 
 // utils.inherits(Toolbar, UIBase);[X]
 export default cls_uiToolbar;
