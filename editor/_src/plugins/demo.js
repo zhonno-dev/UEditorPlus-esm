6fde1cf920fd9ed152cd8b37f4f8518ef3687e14
朱得 <1153468993@qq.com>
2025-03-18 09:05:44 +0800

    refactor(editor): 重构 basestyle 和 paragraph 插件
    - 优化了 basestyle 插件的代码结构，提高了代码的可读性和可维护性
    - 重构了 paragraph 插件的执行逻辑，增强了代码的健壮性
    - 调整了 demo 插件的实现方式，使其与其他插件保持一致

--- editor/_src/plugins/demo.js
+++ editor/_src/plugins/demo.js
@@ -1,58 +1,7 @@
 //import一些文件用于IDE感知
 import cls_Editor from '../core/Editor.cls.js';
 import utils from '../core/utils.js';
-
-/**
- * 新版本的插件注册：使用 UE.plugin.register(插件名, ...)
- * 如 ./anchor.js
- */
-UE.plugin.register("anchor", function () {
-	return {
-		commands: {
-			insertfile: {
-				execCommand: function (command, filelist) {
-					filelist = utils.isArray(filelist) ? filelist : [filelist];
-
-					if (me.fireEvent("beforeinsertfile", filelist) === true) {
-						return;
-					}
-
-
-					//console.log('themePath',  );
-					var i,
-						item,
-						icon,
-						title,
-						html = "",
-						URL = me.getOpt("UEDITOR_HOME_URL"),
-						iconDir = me.options.themePath + me.options.theme + "/exts/";
-					for (i = 0; i < filelist.length; i++) {
-						item = filelist[i];
-						icon = iconDir + getFileIcon(item.url);
-						title =
-							item.title || item.url.substr(item.url.lastIndexOf("/") + 1);
-						html +=
-							'<p>' +
-							'<a style="background:#EEE;padding:10px;border-radius:5px;line-height:1.5em;display:inline-flex;align-items:center;" href="' +
-							item.url +
-							'" title="' +
-							title +
-							'" target="_blank">' +
-							'<img style="vertical-align:middle;margin-right:0.5em;height:1.5em;" src="' + icon + '" _src="' + icon + '" />' +
-							'<span style="color:#111111;line-height:1.5em;flex-grow:1;">' +
-							title +
-							"</span>" +
-							"</a>" +
-							"</p>";
-					}
-					me.execCommand("insertHtml", html);
-
-					me.fireEvent("afterinsertfile", filelist);
-				}
-			}
-		}
-	};
-});
+import { domUtils } from '../core/domUtils.js';
 
 /**
  * 旧版本的插件：UE.plugins["basestyle"] = function () {
@@ -116,11 +65,27 @@ UE.plugins["basestyle"] = function () {
 
 			me.fireEvent("afterinsertaudio", audioObjs);
 		},
+		//zhu:queryCommandState说明
+		//这个queryCommandState是用于查询光标当前的内容是否已是此样式
+		//如果return true则表示是此样式，然后对应的工具栏按钮就会显示成【高亮】的状态。return false 工具栏按钮就是【默认】状态不高亮。
 		queryCommandState: function () {
 			var img = me.selection.getRange().getClosedNode(),
 				flag = img &&
 					(img.className == "edui-audio-audio" || img.className.indexOf("edui-audio-audio") != -1);
 			return flag ? 1 : 0;
+		},
+		//zhu:queryCommandValue 说明
+		//这个queryCommandValue是用于那种 下拉框 工具栏按钮的（比如 段落样式，字体，代码段 这些下拉框），用于返回当前的选中值，以便于下拉框自动切换到对应的值。
+		//比如当前段落是 h1,那就返回 h1 ，然后【段落样式】那个下拉框就会自动变成 h1 选项。
+		/**
+		 * @this {typeof import('../core/Editor.cls.js').default.prototype}
+		 */
+		queryCommandValue: function () {
+			var node = domUtils.filterNodeList(
+				this.selection.getStartElementPath(),
+				"p h1 h2 h3 h4 h5 h6"
+			);
+			return node ? node.tagName.toLowerCase() : "";
 		}
 	};
 	me.commands[cmd] = {
@@ -163,6 +128,59 @@ UE.plugins["basestyle"] = function () {
 		}
 	};
 };
+
+
+/**
+ * 新版本的插件注册：使用 UE.plugin.register(插件名, ...)
+ * 如 ./anchor.js
+ */
+UE.plugin.register("anchor", function () {
+	return {
+		commands: {
+			insertfile: {
+				execCommand: function (command, filelist) {
+					filelist = utils.isArray(filelist) ? filelist : [filelist];
+
+					if (me.fireEvent("beforeinsertfile", filelist) === true) {
+						return;
+					}
+
+
+					//console.log('themePath',  );
+					var i,
+						item,
+						icon,
+						title,
+						html = "",
+						URL = me.getOpt("UEDITOR_HOME_URL"),
+						iconDir = me.options.themePath + me.options.theme + "/exts/";
+					for (i = 0; i < filelist.length; i++) {
+						item = filelist[i];
+						icon = iconDir + getFileIcon(item.url);
+						title =
+							item.title || item.url.substr(item.url.lastIndexOf("/") + 1);
+						html +=
+							'<p>' +
+							'<a style="background:#EEE;padding:10px;border-radius:5px;line-height:1.5em;display:inline-flex;align-items:center;" href="' +
+							item.url +
+							'" title="' +
+							title +
+							'" target="_blank">' +
+							'<img style="vertical-align:middle;margin-right:0.5em;height:1.5em;" src="' + icon + '" _src="' + icon + '" />' +
+							'<span style="color:#111111;line-height:1.5em;flex-grow:1;">' +
+							title +
+							"</span>" +
+							"</a>" +
+							"</p>";
+					}
+					me.execCommand("insertHtml", html);
+
+					me.fireEvent("afterinsertfile", filelist);
+				}
+			}
+		}
+	};
+});
 
 /**
  * 还有一个 registerUI()\
