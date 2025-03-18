05443ec3f3ff3f96b7b59ae1b50f799b936e9e5c
朱得 <1153468993@qq.com>
2025-03-18 15:01:21 +0800

    ~

--- editor/_src/ui/stateful.js
+++ editor/_src/ui/stateful.js
@@ -5,18 +5,29 @@ import { domUtils } from "../core/domUtils.js";
 import uiUtils from "./uiutils.js";
 import cls_UIBase from "./UIBase.js";
 
+/**
+ * 定义一个全局模板字符串 TPL_STATEFUL，用于绑定鼠标事件
+ * 该模板字符串根据浏览器类型（IE 或其他）绑定不同的鼠标事件
+ */
 var TPL_STATEFUL =
 	'onmousedown="$$.Stateful_onMouseDown(event, this);"' +
 	' onmouseup="$$.Stateful_onMouseUp(event, this);"' +
+	// 如果是 IE 浏览器，使用 onmouseenter 和 onmouseleave 事件
 	(browser.ie
 		? ' onmouseenter="$$.Stateful_onMouseEnter(event, this);"' +
 		' onmouseleave="$$.Stateful_onMouseLeave(event, this);"'
+		// 非 IE 浏览器，使用 onmouseover 和 onmouseout 事件
 		: ' onmouseover="$$.Stateful_onMouseOver(event, this);"' +
 		' onmouseout="$$.Stateful_onMouseOut(event, this);"');
 
+/**
+ * 该类用于管理 UI 元素的状态，如悬停、激活、禁用等
+ */
 class cls_uiStateful extends cls_UIBase {
+	/** 元素是否始终可悬停 */
 	alwalysHoverable = false;
-	target = null; //目标元素和this指向dom不一样
+	/** 目标元素，与 this 指向的 DOM 元素不同 */
+	target = null; 
 
 
 	Stateful_init() {
@@ -31,25 +42,24 @@ class cls_uiStateful extends cls_UIBase {
 	// 	return tpl.replace(/stateful/g, function () {
 	// 		return TPL_STATEFUL;
 	// 	});
-	// };
-	// 改成新的 Stateful_getHtmlTpl，通过参数传入 tpl
+	// }
+	/** 成新的 Stateful_getHtmlTpl，通过参数传入 tpl */
 	Stateful_getHtmlTpl(tpl) {
-		// var tpl = this._Stateful_dGetHtmlTpl();
 		// 使用function避免$转义
 		return tpl.replace(/stateful/g, function () {
 			return TPL_STATEFUL;
 		});
 	}
-	//ie专用 = Stateful_onMouseOver
 	Stateful_onMouseEnter(evt, el) {
+		//ie专用 = Stateful_onMouseOver
 		this.target = el;
 		if (!this.isDisabled() || this.alwalysHoverable) {
 			this.addState("hover");
 			this.fireEvent("over"); //zhu:这个事件没人注册
 		}
 	}
-	//ie专用 = Stateful_onMouseOut
 	Stateful_onMouseLeave(evt, el) {
+		//ie专用 = Stateful_onMouseOut
 		if (!this.isDisabled() || this.alwalysHoverable) {
 			this.removeState("hover");
 			this.removeState("active");
